9d803f2def1c34e20107dc8e77a2b7a278e3a9be
Donnie Alimov <mralimov89@gmail.com>
2021-08-07 22:25:27 -0400

    stling navbar

--- real-estate-website/src/components/Navbar.js
+++ real-estate-website/src/components/Navbar.js
@@ -1,27 +1,72 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components/macro';
 import { Link } from 'react-router-dom';
 import { menuData } from '../data/MenuData';
+import { Button } from './Button';
 
 const Nav = styled.nav`
   height: 60px;
-  background: red;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  background: #000;
+`;
+
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  padding: 0 1rem;
+  align-items: center;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
 `;
 
 const Logo = styled(Link)`
+  ${NavLink}
   color: #ffff;
+  font-style: italic;
+  font-size: 2rem;
 `;
 
 const MenuBars = styled.i``;
 
-const NavMenu = styled.div``;
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-rigth: -48px;
+`;
 
+const NavMenuLinks = styled(Link)`
+  ${NavLink}
+  color: #fff;
+  font-size: 1.6rem;
+`;
+
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+`;
 const Navbar = () => {
   return (
     <Nav>
       <Logo>REMAX</Logo>
       <MenuBars />
-      <NavMenu></NavMenu>
+      <NavMenu>
+        {menuData.map((item, i) => (
+          <NavMenuLinks key={item.title} to={item.link}>
+            {item.title}
+          </NavMenuLinks>
+        ))}
+      </NavMenu>
+      <NavBtn>
+        <Button to='/contact' primary='true'>
+          Contact Us
+        </Button>
+      </NavBtn>
       <h1>NavBar</h1>
     </Nav>
   );
