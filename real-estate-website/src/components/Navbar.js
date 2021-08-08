eea491e2e7713383b0a00629af3adbc03ad535e6
Donnie Alimov <mralimov89@gmail.com>
2021-08-07 22:49:19 -0400

    update navbar

--- real-estate-website/src/components/Navbar.js
+++ real-estate-website/src/components/Navbar.js
@@ -3,6 +3,8 @@ import styled, { css } from 'styled-components/macro';
 import { Link } from 'react-router-dom';
 import { menuData } from '../data/MenuData';
 import { Button } from './Button';
+import Bars from '../images/bars.svg';
+// import { FaBars } from 'react-icons/fa';
 
 const Nav = styled.nav`
   height: 60px;
@@ -32,12 +34,29 @@ const Logo = styled(Link)`
   font-size: 2rem;
 `;
 
-const MenuBars = styled.i``;
+const MenuBars = styled.i`
+  @media screen and (max-width: 768px) {
+    display: block;
+    background-image: url(${Bars});
+    background-size: contain;
+    height: 40px;
+    width: 40px;
+    cursor: pointer;
+    //     position: absolute;
+    top: 0;
+    rigth: 0;
+    transform: tranlate(-50%, 25%);
+  }
+`;
 
 const NavMenu = styled.div`
   display: flex;
   align-items: center;
   margin-rigth: -48px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const NavMenuLinks = styled(Link)`
@@ -49,6 +68,10 @@ const NavMenuLinks = styled(Link)`
 const NavBtn = styled.div`
   display: flex;
   align-items: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 const Navbar = () => {
   return (
@@ -67,7 +90,6 @@ const Navbar = () => {
           Contact Us
         </Button>
       </NavBtn>
-      <h1>NavBar</h1>
     </Nav>
   );
 };
