5ce4d62d61a9106d9496d27242daad57ed5b6570
Donnie Alimov <mralimov89@gmail.com>
2021-08-08 18:34:19 -0400

    setup app js

--- real-estate-website/src/App.js
+++ real-estate-website/src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import DropDown from './components/DropDown';
 import Hero from './components/Hero';
 import InfoSection from './components/InfoSection';
@@ -6,6 +6,11 @@ import Navbar from './components/Navbar';
 import { ContenData } from './data/ContentData';
 import { SliderData } from './data/SliderData';
 import GlobalStyle from './globalSyle';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Footer from './components/Footer';
+import DetailsSection from './components/DetailsSection';
+import { DetailsData } from './data/DetailsData';
+import { FooterData } from './data/FooterData';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,13 +19,15 @@ function App() {
     setIsOpen(!isOpen);
   };
   return (
-    <>
+    <Router>
       <GlobalStyle />
       <Navbar toggle={toggle} />
       <DropDown isOpen={isOpen} toggle={toggle} />
       <Hero slides={SliderData} />
       <InfoSection {...ContenData} />
-    </>
+      <DetailsSection cards={DetailsData} />
+      <Footer footerData={FooterData} />
+    </Router>
   );
 }
 
