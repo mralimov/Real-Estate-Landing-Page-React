29a60bc36de533e87df0b1f439098c23919459cc
Donnie Alimov <mralimov89@gmail.com>
2021-08-07 23:45:14 -0400

    update App.js

--- real-estate-website/src/App.js
+++ real-estate-website/src/App.js
@@ -1,12 +1,13 @@
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import { SliderData } from './data/SliderData';
 import GlobalStyle from './globalSyle';
 function App() {
   return (
     <>
       <GlobalStyle />
       <Navbar />
-      <Hero />
+      <Hero slides={SliderData} />
     </>
   );
 }
