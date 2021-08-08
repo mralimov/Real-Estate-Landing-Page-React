acc65c71a38d8835d74064f0982d3456b1959374
Donnie Alimov <mralimov89@gmail.com>
2021-08-08 08:08:48 -0400

    DropDown menu functionality

--- real-estate-website/src/App.js
+++ real-estate-website/src/App.js
@@ -1,13 +1,21 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
+import DropDown from './components/DropDown';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import { SliderData } from './data/SliderData';
 import GlobalStyle from './globalSyle';
+
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <Fragment>
       <GlobalStyle />
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <DropDown isOpen={isOpen} toggle={toggle} />
       <Hero slides={SliderData} />
     </Fragment>
   );
