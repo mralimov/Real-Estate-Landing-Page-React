59e191ef48b2d0d4afe5904c006f398ad47bd5e0
Donnie Alimov <mralimov89@gmail.com>
2021-08-07 23:45:05 -0400

    CarouselData

---
+++ real-estate-website/src/data/SliderData.js
@@ -0,0 +1,39 @@
+import House1 from '../images/House1.jpg';
+import House2 from '../images/House2.jpg';
+import House3 from '../images/House3.jpg';
+import House4 from '../images/House4.jpg';
+
+export const SliderData = [
+  {
+    title: 'Luxury Villa in bali, Indonesia',
+    price: '$4,275,000',
+    path: '/home',
+    label: 'View Home',
+    image: House1,
+    alt: 'House',
+  },
+  {
+    title: 'Luxury Villa in bali, Indonesia',
+    price: '$4,275,000',
+    path: '/home',
+    label: 'View Home',
+    image: House2,
+    alt: 'House',
+  },
+  {
+    title: 'Luxury Villa in bali, Indonesia',
+    price: '$4,275,000',
+    path: '/home',
+    label: 'View Home',
+    image: House3,
+    alt: 'House',
+  },
+  {
+    title: 'Luxury Villa in bali, Indonesia',
+    price: '$4,275,000',
+    path: '/home',
+    label: 'View Home',
+    image: House4,
+    alt: 'House',
+  },
+];
