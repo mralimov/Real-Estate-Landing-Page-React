032e1416dce4033a8e5967c0454cef538f77d744
Donnie Alimov <mralimov89@gmail.com>
2021-08-08 08:50:43 -0400

    ContentData

---
+++ real-estate-website/src/data/ContentData.js
@@ -0,0 +1,26 @@
+import House6 from '../images/House6.jpg';
+import House7 from '../images/House7.jpg';
+
+export const ContenData = {
+  heading: 'Explore our beautiful homes',
+  paragraphOne:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, repellendus accusantium quis modi consequatur atque molestiae. Corporis sit quos veritatis? ',
+  paragraphtwo:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, repellendus accusantium quis modi consequatur atque molestiae. Corporis sit quos veritatis?',
+  buttonLabel: 'View Homes',
+  image: House6,
+  reserve: false,
+  delay: 100,
+};
+
+export const ContenDataTwo = {
+  heading: 'Modern Designed homes',
+  paragraphOne:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, repellendus accusantium quis modi consequatur atque molestiae. Corporis sit quos veritatis? ',
+  paragraphtwo:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, repellendus accusantium quis modi consequatur atque molestiae. Corporis sit quos veritatis?',
+  buttonLabel: 'View Homes',
+  image: House7,
+  reserve: true,
+  delay: 300,
+};
